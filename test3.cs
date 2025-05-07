921af90f299476053298572d624da7aab014266a
geekwithmuskle <oniayobami100@gmail.com>
2025-05-07 03:08:19 +0100

    Create test3.cs

---
+++ test3.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectConsole_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
